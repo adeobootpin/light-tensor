c022c677e9a2394502fa1ad810b021e7a8a777b2
adeobootpin <adeo@bootpin.com>
2021-03-23 14:58:32 -0500

    added copyright notices

--- l-ten/tensor.h
+++ l-ten/tensor.h
@@ -1,6 +1,30 @@
 #ifndef TENSOR_H
 #define TENSOR_H
 
+/*
+MIT License
+
+Copyright (c) 2021 adeobootpin
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+
 #include "shared_pointer.h"
 #include "tensorimpl.h"
 
@@ -852,9 +876,9 @@ namespace lten {
 		}
 
 
-		friend Tensor operator*(float fScalar, Tensor& rhs)
+		friend Tensor operator*(float scalar, Tensor& rhs)
 		{
-			return rhs * fScalar;
+			return rhs * scalar;
 		}
 
 		Tensor reshape(const std::initializer_list<uint64_t>& dims)
